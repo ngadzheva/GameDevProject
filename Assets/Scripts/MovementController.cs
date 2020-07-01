569fd42095523930d2a2a354b9bfe6e79632c408
Seloth <37912222+Seloth@users.noreply.github.com>
2020-07-01 10:51:58 +0300

    Fix Player death

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,130 +7,131 @@ using static PlayerWeaponInventory;
 public class MovementController : MonoBehaviour
 {
 
-  [SerializeField]
-  [Range(0, 5)]
-  private float moveSpeed = 2;
+    [SerializeField]
+    [Range(0, 5)]
+    private float moveSpeed = 2;
 
-  private readonly float movementThreshold = 0.01f;
+    private readonly float movementThreshold = 0.01f;
 
-  private Vector2 velocity = Vector2.zero;
-  public Vector2 Velocity { get => velocity; }
+    private Vector2 velocity = Vector2.zero;
+    public Vector2 Velocity { get => velocity; }
 
-  private new Rigidbody2D rigidbody;
-  private Animator animator;
-  private Animator weaponAnimator;
+    private new Rigidbody2D rigidbody;
+    private Animator animator;
+    private Animator weaponAnimator;
 
-  public UISlider healthbar;
-  public UISlider power;
+    public UISlider healthbar;
+    public UISlider power;
 
-  void Start()
-  {
-    rigidbody = GetComponent<Rigidbody2D>();
-    animator = GetComponent<Animator>();
-    weaponAnimator = GameObject.FindWithTag("Gun").GetComponent<Animator>();
-  }
-
-  void Update()
-  {
-    ResolveLookDirection();
-    Move();
-
-    if (!weaponAnimator)
+    void Start()
     {
-      weaponAnimator = GameObject.FindWithTag("Gun").GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        weaponAnimator = GameObject.FindWithTag("Gun").GetComponent<Animator>();
     }
 
-    if (Input.GetKeyDown(fireKey))
+    void Update()
     {
-      animator.SetTrigger("ShouldAttack");
-      weaponAnimator.SetTrigger("ShouldAttack");
-    }
-  }
+        ResolveLookDirection();
+        Move();
 
-  private void FixedUpdate()
-  {
-    DoHorizontalMove();
-    DoVerticalMove();
-  }
+        if (!weaponAnimator)
+        {
+            weaponAnimator = GameObject.FindWithTag("Gun").GetComponent<Animator>();
+        }
 
-  private void Move()
-  {
-    Vector2 newPosition = new Vector2
-    {
-      x = velocity.x * moveSpeed,
-      y = velocity.y * moveSpeed
-    } * Time.deltaTime + rigidbody.position;
-    rigidbody.MovePosition(newPosition);
-  }
-
-  public void DoHorizontalMove()
-  {
-    float horizontalMoveDirection = Input.GetAxisRaw(HorizontalMovementAxis);
-    SetHorizontalMoveDirection(horizontalMoveDirection);
-    animator.SetFloat("HorizontalMovement", Abs(horizontalMoveDirection));
-    weaponAnimator.SetTrigger("ShouldWalk");
-  }
-
-  public void DoVerticalMove()
-  {
-    float verticalMoveDirection = Input.GetAxisRaw(VerticalMovementAxis);
-    SetVerticalMoveDirection(verticalMoveDirection);
-    animator.SetFloat("VerticalMovement", Abs(verticalMoveDirection));
-    weaponAnimator.SetTrigger("ShouldWalk");
-  }
-
-  public void SetHorizontalMoveDirection(float amount)
-  {
-    velocity.x = amount;
-  }
-
-  public void SetVerticalMoveDirection(float amount)
-  {
-    velocity.y = amount;
-  }
-
-  private void ResolveLookDirection()
-  {
-    if (Abs(velocity.x) > movementThreshold)
-    {
-      SpriteRenderer spriteRenderer = transform.GetComponent<SpriteRenderer>();
-      spriteRenderer.flipX = velocity.x < 0;
-    }
-  }
-
-  public void TurnTowards(float direction)
-  {
-    transform.localScale = new Vector3(Sign(direction), 1, 1);
-  }
-
-  private void OnCollisionEnter2D(Collision2D collision)
-  {
-    GameObject other = collision.gameObject;
-
-    if (other.CompareTag("Blood"))
-    {
-      healthbar.SetValue(5);
-    }
-    else if (other.CompareTag("Power"))
-    {
-      power.SetValue(5);
-    }
-    else if (other.CompareTag("Bullet"))
-    {
-      AmmoAdd(AmmoType.pistolAmmo, 5);
-      // update UI
-    }
-    else if (other.CompareTag("Bullet1"))
-    {
-      AmmoAdd(AmmoType.rifleAmmo, 5);
-      // update UI
-    }
-    else if (other.CompareTag("Bullet2"))
-    {
-      AmmoAdd(AmmoType.shotgunAmmo, 5);
-      // update UI
+        if (Input.GetKeyDown(fireKey))
+        {
+            animator.SetTrigger("ShouldAttack");
+            weaponAnimator.SetTrigger("ShouldAttack");
+        }
     }
 
-    Destroy(other);
-  }
+    private void FixedUpdate()
+    {
+        DoHorizontalMove();
+        DoVerticalMove();
+    }
+
+    private void Move()
+    {
+        Vector2 newPosition = new Vector2
+        {
+            x = velocity.x * moveSpeed,
+            y = velocity.y * moveSpeed
+        } * Time.deltaTime + rigidbody.position;
+        rigidbody.MovePosition(newPosition);
+    }
+
+    public void DoHorizontalMove()
+    {
+        float horizontalMoveDirection = Input.GetAxisRaw(HorizontalMovementAxis);
+        SetHorizontalMoveDirection(horizontalMoveDirection);
+        animator.SetFloat("HorizontalMovement", Abs(horizontalMoveDirection));
+        weaponAnimator.SetTrigger("ShouldWalk");
+    }
+
+    public void DoVerticalMove()
+    {
+        float verticalMoveDirection = Input.GetAxisRaw(VerticalMovementAxis);
+        SetVerticalMoveDirection(verticalMoveDirection);
+        animator.SetFloat("VerticalMovement", Abs(verticalMoveDirection));
+        weaponAnimator.SetTrigger("ShouldWalk");
+    }
+
+    public void SetHorizontalMoveDirection(float amount)
+    {
+        velocity.x = amount;
+    }
+
+    public void SetVerticalMoveDirection(float amount)
+    {
+        velocity.y = amount;
+    }
+
+    private void ResolveLookDirection()
+    {
+        if (Abs(velocity.x) > movementThreshold)
+        {
+            SpriteRenderer spriteRenderer = transform.GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = velocity.x < 0;
+        }
+    }
+
+    public void TurnTowards(float direction)
+    {
+        transform.localScale = new Vector3(Sign(direction), 1, 1);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;
+
+        if (other.CompareTag("Blood"))
+        {
+            healthbar.SetValue(5);
+            Destroy(other);
+        }
+        else if (other.CompareTag("Power"))
+        {
+            power.SetValue(5);
+            Destroy(other);
+        }
+        else if (other.CompareTag("Bullet"))
+        {
+            AmmoAdd(AmmoType.pistolAmmo, 5);
+            Destroy(other);
+        }
+        else if (other.CompareTag("Bullet1"))
+        {
+            AmmoAdd(AmmoType.rifleAmmo, 5);
+            Destroy(other);
+        }
+        else if (other.CompareTag("Bullet2"))
+        {
+            AmmoAdd(AmmoType.shotgunAmmo, 5);
+            Destroy(other);
+        }
+
+}
 }
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -7,58 +7,59 @@ using static ScreenShaker;
 public class PlayerHealth : MonoBehaviour
 {
 
-  [SerializeField]
-  [Range(1, 5)]
-  protected int hp = 5;
+    [SerializeField]
+    [Range(1, 5)]
+    protected int hp = 5;
 
-  [SerializeField]
-	[Range(0.0f, 1.0f)]
-	private float duration = 0.15f;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float duration = 0.15f;
 
-	[SerializeField]
-	[Range(0.0f, 1.0f)]
-	private float magnitude = 0.1f;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float magnitude = 0.1f;
 
-  private Animator animator;
+    private Animator animator;
 
-  public UISlider healthBar;
-  public ParticleSystem blood;
-  public ScreenShaker screenShaker;
+    public UISlider healthBar;
+    public ParticleSystem blood;
+    public ScreenShaker screenShaker;
 
-  void Start()
-  {
-    animator = GetComponent<Animator>();
-    healthBar.SetMaxValue(hp);
-  }
-
-  private void OnTriggerEnter2D(Collider2D collision)
-  {
-    if (collision.CompareTag("Bullet_Enemy"))
+    void Start()
     {
-      TakeDamage();
+        animator = GetComponent<Animator>();
+        healthBar.SetMaxValue(hp);
     }
-  }
 
-  protected void TakeDamage()
-  {
-    hp -= 1;
-    animator.SetTrigger("TookDamage");
-    animator.SetInteger("Health", hp);
-    healthBar.SetValue(hp);
-    PlayEffects();
-
-    if (hp <= 0)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-      PlayDeathSound();
-      screenShaker.StartShake(duration, magnitude);
-      Destroy(gameObject);
+        if (collision.CompareTag("Bullet_Enemy"))
+        {
+            TakeDamage();
+        }
     }
-  }
 
-  private void PlayEffects() {
-		blood.Stop();
-		blood.Play();
-    PlayHitSound();
-		screenShaker.StartShake(duration, magnitude);
-	}
+    protected void TakeDamage()
+    {
+        hp -= 1;
+        animator.SetTrigger("TookDamage");
+        animator.SetInteger("Health", hp);
+        healthBar.SetValue(hp);
+        PlayEffects();
+
+        if (hp <= 0)
+        {
+            PlayDeathSound();
+            screenShaker.StartShake(duration, magnitude);
+            Destroy(gameObject);
+        }
+    }
+
+    private void PlayEffects()
+    {
+        blood.Stop();
+        blood.Play();
+        PlayHitSound();
+        screenShaker.StartShake(duration, magnitude);
+    }
 }
