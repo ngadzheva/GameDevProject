64f577b5ee2eb9ac9802e78987759d230284c3ae
Seloth <37912222+Seloth@users.noreply.github.com>
2020-07-02 19:09:13 +0300

    Fixed wrong type

--- Assets/Scripts/AnimatorStates/HurtState.cs
+++ Assets/Scripts/AnimatorStates/HurtState.cs
@@ -6,7 +6,7 @@ public class HurtState : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (animator.GetFloat("Health") <= 0)
+        if (animator.GetInteger("Health") <= 0)
         {
             animator.SetBool("IsDying", true);
 
