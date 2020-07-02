1c1e51c259aa9a410a4bb4ad261c657e02b088b4
Seloth <37912222+Seloth@users.noreply.github.com>
2020-07-02 19:04:27 +0300

    Reverted HurtState
    This fixes enemyes not dying when continiously beeing shot!!!!!!!!!!!!!!

---
+++ Assets/Scripts/AnimatorStates/HurtState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtState : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.GetFloat("Health") <= 0)
+        {
+            animator.SetBool("IsDying", true);
+
+        }
+    }
+}
